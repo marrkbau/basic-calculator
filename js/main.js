ef6710e4ae68bea0627c3d3c517fa9a6531c7475
B-Mar004 <bmarquez@frba.utn.edu.ar>
2024-01-30 06:09:31 -0300

    puliendo

--- js/main.js
+++ js/main.js
@@ -5,8 +5,38 @@ const botones = document.querySelectorAll(".btn");
 botones.forEach(boton => {
     boton.addEventListener("click", () => {
         const botonApretado = boton.textContent;
+
+        if(boton.id === "c"){
+            pantalla.textContent = "0";
+            return;
+        }
+
+        if(boton.id === "por"){
+            pantalla.textContent = pantalla.textContent.replace("x", "*");
+        }
+
+        if(boton.id === "del"){
+            if(pantalla.textContent.length === 1 || pantalla.textContent === "Syntax Error"){
+                pantalla.textContent = "0";
+            }else {
+                pantalla.textContent = pantalla.textContent.slice(0, -1);
+                
+            }
+            return;
+        }
         
-        if(pantalla.textContent === "0"){
+        if(boton.id === "igual"){
+
+            try {
+                pantalla.textContent = eval(pantalla.textContent);
+            } catch {
+                pantalla.textContent = "Syntax Error"
+            }
+            
+            return; 
+        }
+        
+        if(pantalla.textContent === "0" || pantalla.textContent === "Syntax Error"){
             pantalla.textContent = botonApretado;
         }else {
             pantalla.textContent += botonApretado;
